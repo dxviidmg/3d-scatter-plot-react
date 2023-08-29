514850faff87e59a1ef9769a4f514ff2f6fdef51
David <dmg_92@hotmail.com>
2023-08-29 11:06:31 -0600

    plot 3d

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Scatter3DPlot from './components/plotly/Plotly';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Scatter3DPlot></Scatter3DPlot>
     </div>
   );
 }
